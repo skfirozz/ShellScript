8a5b2beef28f96f1b9b8bb9847c58d7bb1bd8e72
skfirozz <shaikfiroz838@gmail.com>
2020-02-24 18:54:22 +0530

    [skfirozz]updated and some lines deleted in flipcoins

--- basic/flipCoins.sh
+++ basic/flipCoins.sh
@@ -1,20 +1,17 @@
-
 #!/bin/bash
 read -p "enter a number: " max
-#max=10
-head=0
-tail=0
-echo $head $tail
+head=0 tail=0
 for (( i=1; i <= $max; ++i ))
 do 
 	rem=$(( $RANDOM % 2 ))
 	if [ $rem -eq 0 ]
 	then
-#  		$head = $head+1
-	echo "hi"
+  	((head=head+1))
 	else
- # 		$tail = $tail+1
-	echo "welcome"
+  		((tail=tail+1))
 	fi
 done
-echo $head $tail
+	echo $head
+	z=$((head / max))
+	res=$((z*100))
+	echo $res
