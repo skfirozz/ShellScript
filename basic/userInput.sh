c1737871a2d83eeb2ec6b372bb868466a2fcf05b
skfirozz <shaikfiroz838@gmail.com>
2020-02-24 17:44:18 +0530

    [skfirozz] updated userInput

--- basic/userInput.sh
+++ basic/userInput.sh
@@ -1,1 +1,10 @@
-echo "welcome to shell script"
+#!/bin/bash
+read -p "enter your name: " name
+echo $name
+length=${#name}
+echo $length
+if [ $length -ge 3 ]; then
+echo "Hello $name, How are you?"
+else 
+	echo "$name has lessthan 3 characters"
+fi
