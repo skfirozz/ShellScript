b9df6eb4e08df4721892b8be8c31840e093cb436
skfirozz <shaikfiroz838@gmail.com>
2020-02-25 18:32:37 +0530

    [skfirozz]updated added conditions and looping

--- Logical/gambler.sh
+++ Logical/gambler.sh
@@ -1,9 +1,22 @@
 read -p "Enter Number of trails: " trails
 read -p "Enter STACK: " stack
 read -p "Enter Goal : " goal
+wins=0 loss=0 
 for (( i=1; i<=$trails; i++ ));do
-   if [ $RANDOM % 2 -eq 0 ]; then
-        echo "even"
-   else echo "odd"
+  rem=$(( $RANDOM % 2)) 
+  if [ $rem -eq 0 ] ; then
+        ((stack=stack+1))
+	((wins=wins+1))
+   else  
+	((stack=stack-1))
+	((loss=loss-1))
    fi
+   if (( $stack == $goal )); then
+	break
+   elif (($stack == 0)); then
+	break
+   fi 
 done
+echo $wins
+echo $stack
+echo $loss
