07250442e9926f5d31f5595f0f5399e2cd8431e0
skfirozz <shaikfiroz838@gmail.com>
2020-02-25 09:31:16 +0530

    [skfirozz]updated leapyear.sh

--- basic/leapYear.sh
+++ basic/leapYear.sh
@@ -1,3 +1,4 @@
+
 echo "enter the year: "
 read year
 if [ $((year % 4)) -eq 0 ] 
@@ -7,13 +8,9 @@ then
     if [ $((year % 400)) -eq 0 ] 
           then 
         echo "its a leap year"
-    else
-           echo "its not a leap year"
+    else echo "its not a leap year"
     fi
-  else
-  echo "Its not a leap year"
+  else echo "Its not a leap year"
   fi
-else
- echo "its not a leap year"
-
+else echo "its not a leap year"
 fi
