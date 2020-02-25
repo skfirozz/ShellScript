e1d486b6f0931425c61cf8004c791071c3fc90de
skfirozz <shaikfiroz838@gmail.com>
2020-02-25 17:50:06 +0530

    [skfirozz]updated quadratic.sh added scale and delta and working

--- Functional/quadratic.sh
+++ Functional/quadratic.sh
@@ -1,5 +1,7 @@
 read -p "Enter A value: " a
 read -p "Enter B value: " b
 read -p "Enter C value: " c
-delta=$b*$b - 4*$a*$c
-echo $delta
+((delta=b*b - 4*a*c))
+delta=$(bc <<< "scale=2; sqrt($delta)")
+echo 'scale=2; -b+delta/2*a' | bc
+echo 'scale=2; -b-delta/2*a' | bc
