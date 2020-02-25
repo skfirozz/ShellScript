5a8db739acf3df041602e6bebf5513eba2ef176a
skfirozz <shaikfiroz838@gmail.com>
2020-02-25 19:32:01 +0530

    [skfirozz]created coupounNumber.sh and added some lines

---
+++ Logical/coupounNumbers.sh
@@ -0,0 +1,9 @@
+read -p "Enter how many coupoun numbers you want: " number
+function check()
+{
+	 array=$1
+	for ((j=0; j<${#array[@]}; j++));do
+		
+}
+declare -a array
+for (( i=0;i<$number; i++))
