7a7a6efea1495a9d81b5783cf46c9649f36fb95b
skfirozz <shaikfiroz838@gmail.com>
2020-02-25 16:30:35 +0530

    [skfirozz]updated and sumOfThree.sh is working

--- Functional/sumOfThree.sh
+++ Functional/sumOfThree.sh
@@ -1,12 +1,18 @@
-function sumOfThree()
-{
-
-}
 echo "Enter a array size: "
 read size
+declare -a array
 for (( i=0; i<$size; i++));do
-  read array[$i]
+	read val
+  	array[$i]=$val
 done
-
-for (( i=0; i<${#array[@]}; i++ ));do
-  for (())
+for (( i=0; i<${#array[@]}; i++ )); do
+  for (( j=i+1; j<${#array[@]}; j++ )); do
+    for (( k=j+1; k<${#array[@]}; k++)); do
+	(( value=${array[i]+array[j]+array[k]} ))
+	echo $value
+	if [ $value -eq 0 ] ; then
+	   echo "$i  $j  $k  "
+	fi
+     done
+   done
+done
