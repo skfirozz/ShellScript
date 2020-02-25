4921534a6d9739cfe61fc8f48011ab53442910a3
skfirozz <shaikfiroz838@gmail.com>
2020-02-25 12:45:39 +0530

    [skfirozz]updated array and testing

--- Functional/twoDimensionalArry.sh
+++ Functional/twoDimensionalArry.sh
@@ -2,13 +2,21 @@ echo "Enter the number of rows: "
 read rows
 echo "Enter the number of columns: "
 read columns
+echo "enter array elements"
+declare -A array
 for (( i=0; i<rows; i++ ))
 do
  for(( j=0; j<columns; j++ ))
   do
- echo "enter $i $j: "
-   read array[$i,$j]
+ read element
+ array[$i,$j]=$element
   done
 done
-echo "${array[1,1]}"
-echo "${#array[@]}"
+echo "array elements are"
+for (( i=0; i<rows; i++ ))
+do
+ for(( j=0; j<columns; j++ ))
+  do
+  echo  ${array[$i,$j]}
+  done
+done
