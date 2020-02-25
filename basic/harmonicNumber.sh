c8a07db4da61f2af260e3dca77f935cb50ab0849
skfirozz <shaikfiroz838@gmail.com>
2020-02-25 11:03:39 +0530

    [skfirozz]updated ->added for loop in harmonicNumber.sh

--- basic/harmonicNumber.sh
+++ basic/harmonicNumber.sh
@@ -1,3 +1,13 @@
 echo "enter a number: "
 read number
-
+value=0.0
+for (( i=1; i<= $number; i++ ))
+do
+# value=$((value+1.0 / i ))
+echo "hi"
+done
+echo $value
+A=333.33
+B=111.11
+C="$B / $A" | bc -l
+echo $c
