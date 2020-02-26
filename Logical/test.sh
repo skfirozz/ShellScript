fc4385bf7f64e55fb8b1d211fb6618f7d8cee5db
skfirozz <shaikfiroz838@gmail.com>
2020-02-26 11:58:32 +0530

    [skfirozz]updated test2.sh

--- Logical/test.sh
+++ Logical/test.sh
@@ -1,20 +1,26 @@
 myfunc()
-{  input1=$1
+{ input1=$1
   input2=$2
+  echo ${#input[@]}
   a="hi this is firoz $input1"
-  b="this is akhilesh and $2 is my second input"
+  b="this is akhilesh and $2 is my second input is ${input[1]}"
 }
 declare -a array
 
 read -p "Enter how many numbers you want: " number
-echo $number
+#echo $number
 for (( i=1; i<=$number; i++));do
-echo $i
+ read array[$i]
 done
+
+
+echo "----------"
+echo ${array[1]}
+echo "--------"
 
 
 a=hello
 b=banglore
-myfunc $a $b
+myfunc $a $array
 echo "a is $a"
 echo "b is $b"
