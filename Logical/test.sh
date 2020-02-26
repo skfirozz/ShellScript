9e516f1867b2db6d8c170a048e003806b3691cf9
skfirozz <shaikfiroz838@gmail.com>
2020-02-26 11:38:37 +0530

    [skfirozz]created test2.sh and updated test.sh

--- Logical/test.sh
+++ Logical/test.sh
@@ -1,11 +1,17 @@
-#!/bin/sh
 myfunc()
-{
-  input1=$1
+{  input1=$1
   input2=$2
   a="hi this is firoz $input1"
-  b="this is akhilesh $input2"
+  b="this is akhilesh and $2 is my second input"
 }
+declare -a array
+
+read -p "Enter how many numbers you want: " number
+echo $number
+for (( i=1; i<=$number; i++));do
+echo $i
+done
+
 
 a=hello
 b=banglore
