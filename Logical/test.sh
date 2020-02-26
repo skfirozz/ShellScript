549a839358f7d23f27f2b973f11a37074cbd2562
skfirozz <shaikfiroz838@gmail.com>
2020-02-26 13:32:33 +0530

    [skfirozz]updated test.sh

--- Logical/test.sh
+++ Logical/test.sh
@@ -1,26 +1,25 @@
-myfunc()
-{ input1=$1
-  input2=$2
-  echo ${#input[@]}
-  a="hi this is firoz $input1"
-  b="this is akhilesh and $2 is my second input is ${input[1]}"
+
+#declare -a array
+#read -p "Enter how many numbers you want: " number
+#for ((i=0;i<$number;i++));do
+#	read ${array[i]}
+#done
+myFunction(){
+  param1=("${!1}")
+  param2=("${!2}")
+  for i in ${param1[@]}
+  do
+    for j in ${param2[@]}
+    do
+       if [ "${i}" == "${j}" ]
+       then
+           echo ${i}
+           echo ${j}
+       fi
+    done
+  done
 }
-declare -a array
 
-read -p "Enter how many numbers you want: " number
-#echo $number
-for (( i=1; i<=$number; i++));do
- read array[$i]
-done
-
-
-echo "----------"
-echo ${array[1]}
-echo "--------"
-
-
-a=hello
-b=banglore
-myfunc $a $array
-echo "a is $a"
-echo "b is $b"
+a=(foo bar baz)
+b=(foo bar qux)
+myFunction a[@] b[@]
