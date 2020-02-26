7c7179acedbb92b97384ef3eca99af0bf89bc45b
skfirozz <shaikfiroz838@gmail.com>
2020-02-26 18:12:20 +0530

    [skfirozz]updated test2.sh and check function is implemented for tictactoe

--- Logical/test2.sh
+++ Logical/test2.sh
@@ -1,19 +1,65 @@
-myFunction(){
-  param1=${!1}
-  param2=${!2}
-  for i in ${param1[@]}
-  do
-    for j in ${param2[@]}
-    do
-       if [ "${i}" == "${j}" ]
-       then
-           echo ${i}
-           echo ${j}
-       fi
-    done
-  done
+check()
+{
+   array="${!1}"
+   for ((i=0;i<3;i++)) ; do
+	count1=0 count2=0
+	for ((j=0; j<3; j++)); do 
+	 if [ "${array[$i,$j]}" -eq 1 ]; then
+	   	((count1=count1+1)) ;fi
+	  if [ "${array[$i,$j]}" -eq 2 ]; then
+	  	((count2=count2+1)) ;fi
+	done
+
+	if [ $count1 -eq 3 ]; then
+	  echo "***win system---------->1***"
+	 break
+	elif [ $count2 -eq 3 ]; then
+	  echo "***you win***--------->1"
+	  break 
+	fi
+   done
+
+
+   for ((j=0;j<3;j++)) ; do
+        count1=0 count2=0
+        for ((i=0; i<3; i++)); do 
+         if [ "${array[$i,$j]}" -eq 1 ]; then
+                ((count1=count1+1)); fi
+          if [ "${array[$i,$j]}" -eq 2 ]; then
+                ((count2=count2+1)) ; fi
+        done
+
+        if [ $count1 -eq 3 ]; then
+          echo "***win system***------->2"
+         break
+        elif [ $count2 -eq 3 ]; then
+          echo "***you win***-------->2"
+          break 
+        fi
+   done
+
+     if [ "${array[0,0]}" == "${array[1,1]}" -a "${array[2,2]}" == 1 ] ; then
+	echo "h1"
+#	return 1
+   elif [ "${array[0,2]}" == "${array[1,1]}" -a "${array[2,0]}" == 1 ] ; then
+	echo "h2"
+#	return 1
+   elif [ "${array[0,0]}" == "${array[1,1]}" -a "${array[2,2]}" == 2 ] ; then
+	echo "h3"
+#	return 2
+   elif [ "${array[0,2]}" == "${array[1,1]}" -a "${array[2,0]}" == 2 ] ; then
+	echo "h4"
+#       return 2
+   fi
 }
 
-a=(foo bar baz)
-b=(foo bar qux)
-myFunction a[@] b[@]
+declare -A array
+read -p "Enter how many numbers you want: " number
+for (( i=0; i<$number; i++));do
+	for (( j=0;j<$number;j++));do
+ 	     read array[$i,$j]
+	done
+done
+
+check array[@]
+
