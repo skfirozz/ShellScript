b11711d67f21730fc93323ccd3be5129b49fb0bb
skfirozz <shaikfiroz838@gmail.com>
2020-02-26 13:18:16 +0530

    [skfirozz]updated and testing on function

--- Logical/test2.sh
+++ Logical/test2.sh
@@ -1,13 +1,15 @@
 myFunction(){
-  param1=("${!1}")
-  param2=("${!2}")
-  for i in ${param1[@]};do
-    for j in ${param2[@]};do
-       if [ "${i}" == "${j}" ];then
+  param1=${!1}
+  param2=${!2}
+  for i in ${param1[@]}
+  do
+    for j in ${param2[@]}
+    do
+       if [ "${i}" == "${j}" ]
+       then
            echo ${i}
            echo ${j}
        fi
-	echo "---"
     done
   done
 }
