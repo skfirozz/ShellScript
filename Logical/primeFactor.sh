f00c0c63e6a734d0980ae0b5e9dbe226b8ca639a
skfirozz <shaikfiroz838@gmail.com>
2020-02-28 15:11:02 +0530

    [skfirozz]added primeFactor function

---
+++ Logical/primeFactor.sh
@@ -0,0 +1,20 @@
+function primeFactor()
+{
+	for ((i=2;i<=$number;i++));do
+		count=0
+		if [ $(($number % $i)) == 0 ];then
+		for ((j=2;j<=$i;j++));do
+			if [ $(($i % $j)) == 0 ];then
+				((count=count+1))
+			fi
+		done
+		fi
+		if [ $count -eq 1 ];then
+			echo -n "$i "
+		fi
+	done
+}
+
+read -p "Enter a number for prime factor: " number
+primeFactor
+echo ""
